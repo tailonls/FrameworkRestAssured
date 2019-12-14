792e41c9a889f98fa0237635429913d24da6b962
tailonls <tailonlimas@gmail.com>
2019-12-14 19:51:50 -0300

    adicionando novas formas de usar o restassures

--- src/main/java/diversos/ExemplosSimples.java
+++ src/main/java/diversos/ExemplosSimples.java
@@ -1,8 +1,9 @@
 package diversos;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
@@ -12,11 +13,27 @@ public class ExemplosSimples {
 	@Test
 	public void teste() {
 		
-		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me:80/ola");
+		Response response = request(Method.GET, "http://restapi.wcaquino.me:80/ola");
 		
-		// Validando retorno
+		// Validando retorno com 'Then'
 		ValidatableResponse validacao = response.then();
-		validacao.statusCode(201);
+		validacao.statusCode(200);
+		
+		// Validando retorno com Assert
+		Assert.assertEquals("Ola Munsdo!", response.getBody().asString());
+		Assert.assertTrue("Status da requisição estava diferente do esperado!", response.getStatusCode() == 200);
+		
+		
+		// Simplificando execução
+		get("http://restapi.wcaquino.me:80/ola").then().statusCode(201);
+		
+		
+		// Validando com codo fluente (Gherkin)
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me:80/ola")
+		.then()
+			.assertThat().statusCode(201); // assertThat() apenas para dar legibilidade
 	}
 
 }
